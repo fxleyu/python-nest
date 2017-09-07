7dedb4379f9567d115cd60261a8a7aa26673dafc
fxleyu <fxleyu@qq.com>
2017-09-07 22:58:50 +0800

    INSERT 添加python2.7学习项目；添加python基础练习代码

---
+++ BeginningPython/python27/imooc177/imooc_ch3.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+# http://www.imooc.com/learn/177
+
+# 3.1
+print 45678 + 0x12fd2
+print "Learn Python in imooc"
+print 100 < 99
+print 0xff == 255
+
+# 3.2
+print "hello, python"
+print "hello,", "python"
+
+# 
+a = 1
+print a
+a = "hello"
+print a
+
+# 3.4
+x1 = 1
+d = 3
+n = 100
+x100 = 1 + (100 - 1) * 3
+s = (x1 + x100) * 50
+print s
+
+# 3.5
+x = 'Python was started in 1989 by "Guido".\nPython is free and easy to learn.'
+print x
+
+# 3.6
+print '"To be, or not to be\": that is the question.\nWhether it\'s nobler in the mind to suffer.'
+print r'''"To be, or not to be": that is the question.
+Whether it's nobler in the mind to suffer.'''
+
+# 3.7
+print u'''静夜思
+床前明月光，
+疑是地上霜。
+举头望明月，
+低头思故乡。'''
+
+# 3.8
+print 2.5 + 10.0 / 4
+
+# 3.9
+a = True
+print 'hello,', a or 'world'
+
+b = ''
+print 'hello,', b or 'world'
